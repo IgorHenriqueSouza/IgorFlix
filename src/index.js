1981f164a212942ec1b8bdb23474f35622335d5e
IgorHenriqueSouza <br.igorhenrique@gmail.com>
2020-07-28 16:02:58 -0300

    Criando a pagina de cadastro de video e de Categoria

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from './App';
+import CadastroVídeo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
 
+
+
+  const Pagina404 = () =>(<div>Página 404</div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+
+      <Switch>
+        <Route path="/" component={App} exact />
+        <Route path="/cadastro/video" component={CadastroVídeo} />
+        <Route path="/cadastro/Categoria" component={CadastroCategoria} />
+        <Route component={Pagina404} />
+      </Switch>
+
+  </BrowserRouter>,
+  
   document.getElementById('root')
 );
 
